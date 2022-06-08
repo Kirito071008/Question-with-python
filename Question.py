2a8cfe9c8c4860664be95f8cbb805cd25f44ad55
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2022-06-08 02:45:57 +0200

    Create Question.py

---
+++ Question.py
@@ -0,0 +1,17 @@
+print("Hello")
+
+question = (input("Do you love me?")) #First question
+if (question<"yes"): 
+    print(":(") #everything you write here will appear if the answer is different
+
+else:
+    print("Yes!") #everything you write here will appear if the answer was equal to (question<yes)
+
+next #use it to separate questions
+
+question2 = (input("For real?")) #Second Question
+if (question<"yes"):
+    print("Fuck") #everything you write here will appear if the answer is different
+
+else:
+    print("<3") #everything you write here will appear if the answer was equal to (yes)
