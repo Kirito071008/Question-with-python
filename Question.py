0042b43e45c682225d3ad7d23e6cda007eaf3f51
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2022-07-12 14:53:57 +0200

    Stop code
    In the line 5 you can interrupt the code if the answer is no.
    I wrote the method in the line 6

--- Question.py
+++ Question.py
@@ -3,10 +3,10 @@ print("Hello")
 question = (input("Do you love me?")) #First question
 if (question<"yes"): 
     print(":(") #everything you write here will appear if the answer is different
-
+    #If you wish, you can interrupt the code here if the answer is no by deleting print (":(") and typing exit ()
 else:
     print("Yes!") #everything you write here will appear if the answer was equal to (question<yes)
-
+    
 next #use it to separate questions
 
 question2 = (input("For real?")) #Second Question
