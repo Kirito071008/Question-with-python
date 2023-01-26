a3e9ce5a6964fa00b390eaf5611a88eab0fc0d1d
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2023-01-26 22:14:42 +0100

    Update
    Honestly, when i first wrote this code, i don't really knew python very well, so this update is for making the script working right

--- Question.py
+++ Question.py
@@ -1,18 +1,19 @@
 print("Hello")
 
 question = (input("Do you love me?")) #First question
-if (question<"yes"): 
-    print(":(") #everything you write here will appear if the answer is different
-    #If you wish, you can interrupt the code here if the answer is no by deleting print (":(") and typing exit ()
+if (question == "yes" or question == "Yes" or question == "YES"): 
+    print("Yes!") #everything you write here will appear if the answer is yes
+    
    
 else:
-    print("Yes!") #everything you write here will appear if the answer was equal to (question<yes)
+    print("Oh...") #everything you write here will appear if the answer was different to yes
+    #If you wish, you can interrupt the code here if the answer is no by deleting print ("Oh...") and typing exit ()
     
-next #use it to separate questions
+next #use it to separate questions, it's not compulsory
 
 question2 = (input("For real?")) #Second Question
-if (question>"yes"):
-    print("Fuck") #everything you write here will appear if the answer is different
+if (question == "yes" or question == "Yes" or question == "YES"):
+    print("<3") #everything you write here will appear if the answer is yes
 
 else:
-    print("<3") #everything you write here will appear if the answer was equal to (question>yes)
+    print("...") #everything you write here will appear if the answer is different to yes
