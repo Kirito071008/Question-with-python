8828cd9ff16efd3630a928f351d4287503b2d146
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2023-12-15 01:30:49 +0100

    Update Question.py

--- Question.py
+++ Question.py
@@ -1,19 +1,13 @@
 print("Hello")
 
-question = (input("Do you love me?")) #First question
-if (question == "yes" or question == "Yes" or question == "YES"): 
+question = input("Do you love me?\n") #First question
+if (question.lower == "yes" or question.lower == "y"):
     print("Yes!") #everything you write here will appear if the answer is yes
-    
-   
 else:
     print("Oh...") #everything you write here will appear if the answer was different to yes
     #If you wish, you can interrupt the code here if the answer is no by deleting print ("Oh...") and typing exit ()
-    
-next #use it to separate questions, it's not compulsory
-
-question2 = (input("For real?")) #Second Question
-if (question == "yes" or question == "Yes" or question == "YES"):
+question2 = input("For real?\n") #Second Question
+if (question2.lower == "yes" or question2.lower == "y"):
     print("<3") #everything you write here will appear if the answer is yes
-
 else:
     print("...") #everything you write here will appear if the answer is different to yes
